e87fde415dbd11836730753315ab0199a24183ed
Kash Karimi <kash.karimi@gmail.com>
2017-06-18 10:09:58 +0100

    Adding pages, Work in progress

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
-import App from './App';
-import Routes from './routes';
+// import App from './App';
+import Navigation from './components/Navigation';
+import Home from './containers/Home'
+import {
+  About,
+  Volunteers,
+  Students,
+  Partners
+} from './Pages'
+
+// import Routes from './routes';
 
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <div>
+      <Navigation />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/students" component={Students}/>
+      <Route path="/volunteers" component={Volunteers}/>
+      <Route path="/partners" component={Partners}/>
+    </div>
   </Router>,
   document.getElementById('root')
 );
