d23a306b45c2c742bc6e4dd81add93bfbf98d560
Kash Karimi <kash.karimi@gmail.com>
2017-06-19 13:12:15 +0100

    Move to separate component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,53 +2,7 @@ import React from 'react'
 
 export const Footer = () => (
   <div className="section-container">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 col-md-6">
-            <h2>As seen in</h2>
-            <div className="divider-1"><span></span></div>
-            <br />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3">
-            <a href="http://www.wired.co.uk/article/codeyourfuture-refugee-coding-school"
-              target="_blank"
-              rel="noopener noreferrer">
-              <div className="icon--large">
-                <img src="img/media-logos/wired.svg" alt="Wired"/>
-              </div>
-            </a>
-        </div>
-        <div className="col-sm-3">
-            <a href="http://www.unhcr.org/news/stories/2017/1/586e420c7/volunteers-train-refugees-to-crack-into-london-tech-industry.html"
-              target="_blank"
-              rel="noopener noreferrer">
-              <div className="icon--large">
-                <img src="img/media-logos/unhcr.png" alt="UNHCR"/>
-              </div>
-            </a>
-        </div>
-        <div className="col-sm-3">
-            <a href="https://www.newsdeeply.com/refugees/articles/2016/10/19/welcome-to-londons-refugee-coding-school"
-              target="_blank"
-              rel="noopener noreferrer">
-              <div className="icon--large">
-                <img src="img/media-logos/refugees-deeply.jpeg" alt="Refugees Deeply" />
-              </div>
-            </a>
-        </div>
-        <div className="col-sm-3">
-            <a href="http://www.bbc.co.uk/programmes/p04yzrrg"
-              target="_blank"
-              rel="noopener noreferrer">
-              <div className="icon--large">
-                <img src="img/media-logos/BBC.svg" alt="BBC" />
-              </div>
-            </a>
-        </div>
-      </div>
-    </div>
+
   <footer>
     <div className="container">
       <div className="row">
@@ -71,7 +25,7 @@ export const Footer = () => (
                 </a>
               </strong>
               &nbsp;|&nbsp;
-              Supported with <img src="img/ico/hearts.svg" alt="LOVE"/>by
+              Supported with <img src="/img/ico/hearts.svg" alt="LOVE"/>by
               <strong>
                 <a href="http://www.hackyourfuture.net"
                   target="_blank"
