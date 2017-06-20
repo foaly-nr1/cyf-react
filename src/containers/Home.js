0dc0168affe8e4e667e869ca9919934475811420
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2017-06-20 10:53:41 +0100

    Add remaining team members

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,6 +8,20 @@ export const Home = () => (
   <div className="Home">
     <div className="App-intro">
       <Banner />
+
+      <div className="row">
+        <div className="col-sm-8 col-sm-offset-2 section-description">
+          <p>We are a non-profit organisation supporting refugees with the dream of becoming developers.</p>
+          <p>In their journey of interrupted lives, unfinished studies and integration challenges, many asylum seekers and refugees
+          yearn to update their tech skills, but lack learning opportunities. <strong>We want to change this.</strong></p>
+          <p>Last year we launched the first cohort of our  6-month web development programme, coached by a group of professional developers. Today we are running new classes in London and Glasgow. This is <strong>just the beginning</strong>. With your help we will be expanding to other regions and cities.</p>
+          <p>If you are interested in participating as a <a href="students.html#info">student</a>, <a href="volunteers.html">coach or volunteer</a>, sign up!</p>
+          <p>
+          For all other inquiries please contact us at <a href="mailto:contact@codeyourfuture.co" title="Contact us">contact@codeyourfuture.co</a>
+          <br /><br />
+          </p>
+        </div>
+      </div>
     </div>
 
     <WhatWeTeach />
--- src/content/mentors/index.js
+++ src/content/mentors/index.js
@@ -1,15 +1,41 @@
+import german from './german.jpg';
+import mozafar from './mozafar.jpg';
 import kash from './kash.jpg';
 import amit from './amit.jpg';
 import simon from './simon.jpg';
 import james from './james.png';
+import bex from './bex.jpg';
 import claudia from './claudia.jpg';
+import nate from './nate.jpg';
+import will from './will.jpg';
 
 const mentors = [
+  {
+    firstName: 'Germán',
+    lastName: 'Bencci',
+    photo: german,
+    title: 'Organiser'
+  },
+
+  {
+    firstName: 'Mozafar',
+    lastName: 'Haider',
+    photo: mozafar,
+    title: 'Scotland Organiser & React'
+  },
+
   {
     firstName: 'Kash',
     lastName: 'Karimi',
     photo: kash,
     title: 'Javascript & React'
+  },
+
+  {
+    firstName: 'Bex',
+    lastName: 'Bolton',
+    photo: bex,
+    title: 'Javascript & NodeJS'
   },
 
   {
@@ -27,6 +53,13 @@ const mentors = [
   },
 
   {
+    firstName: 'Claudia',
+    lastName: 'Matosa',
+    photo: claudia,
+    title: 'HTML, CSS & React'
+  },
+
+  {
     firstName: 'James',
     lastName: 'Wright',
     photo: james,
@@ -34,10 +67,17 @@ const mentors = [
   },
 
   {
-    firstName: 'Claudia',
-    lastName: 'Matosa',
-    photo: claudia,
-    title: 'HTML, CSS & React'
+    firstName: 'Nate',
+    lastName: 'Wright',
+    photo: nate,
+    title: 'HTML & CSS'
+  },
+
+  {
+    firstName: 'Will',
+    lastName: '',
+    photo: will,
+    title: 'HTML, CSS & Javascript'
   }
 ];
 
