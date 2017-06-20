9a718bd1bc4b201523f47cdb5d348ab806b72233
Kash Karimi <kash.karimi@gmail.com>
2017-06-20 22:47:04 +0100

    Fix test

--- src/Index.test.js
+++ src/Index.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Home } from './containers/Home';
+import { Routes } from './Routes';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
+  ReactDOM.render(<Routes />, div);
 });
