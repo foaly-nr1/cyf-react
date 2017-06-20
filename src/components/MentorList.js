f2dca3008f1f24e2769ba71ba20b11d9d2f0f8bc
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2017-06-20 21:54:43 +0100

    Display 'Become a volunteer' button after the list of volunteers

--- src/components/MentorList.js
+++ src/components/MentorList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { MentorCard } from './MentorCard';
 import mentors from '../content/mentors';
-import mediaQueries from '../variables/media-queries'
+import mediaQueries from '../variables/media-queries';
 
 const Mentors = styled.section`
   display: flex;
@@ -20,8 +21,24 @@ const Mentor = styled(MentorCard)`
   }
 `;
 
+const CallToActionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1 auto;
+  padding: 2em 2em 8em 3em;
+`;
+
+const CallToAction = styled(Link)`
+  white-space: normal;
+`;
+
 export const MentorList = () => (
   <Mentors id="mentors">
     {mentors.map(mentor => <Mentor {...mentor} />)}
+
+    <CallToActionContainer>
+      <CallToAction className="big-link-3 btn" to="/volunteers">Become a volunteer</CallToAction>
+    </CallToActionContainer>
   </Mentors>
 );
--- src/pages/MeetTheTeam.js
+++ src/pages/MeetTheTeam.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import { TopSection } from '../components/TopSection';
 import { MentorList } from '../components/MentorList';
@@ -10,9 +10,9 @@ const Intro = (
       Last year a small group of professional developers got together with a group of asylum seekers and refugees to start the first class of CodeYourFuture. Today we are small community creating an environment that is open, playful and tolerant, ensuring a lasting learning experience that influences the lives of our students.
     </p>
 
-    <div className="section-bottom-link">
-      <Link className="big-link-3 btn" to="/volunteers">Become a volunteer</Link>
-    </div>
+    <p>
+      Below are some of the wonderful people that make this project happen. Many more help our students every week.
+    </p>
   </section>
 );
 
