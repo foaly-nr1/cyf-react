ccb15cdd0bae97510e6dbde8d37808cd393a0b87
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2017-06-25 17:08:36 +0100

    ✨ Add React logo to 'What we teach'

--- src/components/WhatWeTeach.js
+++ src/components/WhatWeTeach.js
@@ -11,26 +11,33 @@ export const WhatWeTeach = () => (
         </div>
         </div>
         <div className="row">
-        <div className="col-sm-4">
+        <div className="col-sm-3">
             <div className="md-icon">
               <img src="img/ico/tablet.svg" alt="Tablet" />
             </div>
             <h4>HTML &amp; CSS</h4>
             <p>Fundamentals of design. Design static web pages using best front-end practices.</p>
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-3">
             <div className="md-icon">
               <img src="img/ico/code.svg" alt="Code" />
             </div>
             <h4>JavaScript</h4>
             <p>Use powerful modern scripting language to build web applications.</p>
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-3">
             <div className="md-icon">
               <img src="img/ico/nodejs.svg" alt="NodeJS" />
             </div>
             <h4>Node.js</h4>
             <p>Web application development powered by modern and fast Node.js</p>
+        </div>
+        <div className="col-sm-3">
+            <div className="md-icon">
+              <img src="img/ico/react.svg" alt="React" />
+            </div>
+            <h4>React</h4>
+            <p>A Javascript library for building user interfaces</p>
         </div>
       </div>
     </div>
