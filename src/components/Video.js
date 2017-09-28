92a06f7fa3a6af1dde6fcaca8675fce738fe9590
Kash <kash.karimi@gmail.com>
2017-09-28 13:17:02 +0100

    responsive embed

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,14 +4,17 @@ export const Video = () => (
   <div className="section-container section-container-gray">
     <div className="container">
         <div className="row">
-            <iframe 
-              title="CodeYourFuture graduate Ehmed shares his story"
-              width="853"
-              height="480"
-              src="https://www.youtube.com/embed/LpQUHt2N6rY?rel=0&amp;showinfo=0"
-              frameBorder="0"
-              allowFullScreen>
-            </iframe>
+        <div className="embed-responsive embed-responsive-16by9" >
+          <iframe 
+            className="embed-responsive-item"
+            title="CodeYourFuture graduate Ehmed shares his story"
+            width="853"
+            height="480"
+            src="https://www.youtube.com/embed/LpQUHt2N6rY?rel=0&amp;showinfo=0"
+            frameBorder="0"
+            allowFullScreen>
+          </iframe>
+        </div>
       </div>
     </div>
 </div>
