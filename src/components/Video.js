a90d562d80d937556993cd1005cd91b64bd6f3c3
Kash <kash.karimi@gmail.com>
2017-09-28 12:39:10 +0100

    Add ehmed video via youtube embed

---
+++ src/components/Video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Video = () => (
+  <div className="section-container section-container-gray">
+    <div className="container">
+        <div className="row">
+            <iframe 
+              title="CodeYourFuture graduate Ehmed shares his story"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/LpQUHt2N6rY?rel=0&amp;showinfo=0"
+              frameborder="0"
+              allowfullscreen>
+            </iframe>
+      </div>
+    </div>
+</div>
+)
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { AsSeen } from '../components/AsSeen'
 import { Banner } from '../components/Banner'
 import { WhatWeTeach } from '../components/WhatWeTeach'
+import { Video } from '../components/Video'
 
 export const Home = () => (
   <div className="Home">
     <div className="App-intro">
       <Banner />
     </div>
-
+    <Video />
     <WhatWeTeach />
     <AsSeen />
   </div>
