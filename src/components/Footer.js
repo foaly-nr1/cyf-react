5324336cb2d6f7bed25ca562991304f4f30a46e3
Kash <kash.karimi@gmail.com>
2017-10-04 23:59:13 +0100

    Add charity number to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
   <div className="section-container">
-
-  <footer>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12">
-          <div className="scroll-to-top">
-            <a href="#top">
-            <i className="fa fa-chevron-up" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="credits">
+    <footer>
+      <div className="container">
+        <div className="row">
           <div className="col-sm-12">
-            <div className="our-vendor">
-              &copy; All rights reserved&nbsp;|&nbsp;Photos by &copy;
-              <strong>
-                <Link to="http://www.alicerowsome.com/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  Alice Rowsome
-                </Link>
-              </strong>
-              &nbsp;|&nbsp;
-              Supported with <img src="/img/ico/hearts.svg" alt="LOVE"/>by
-              <strong>
-                <Link to="http://www.hackyourfuture.net"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  HackYourFuture
-                </Link>
-              </strong>
+            <div className="scroll-to-top">
+              <a href="#top">
+                <i className="fa fa-chevron-up" />
+              </a>
             </div>
-            <div className="vendor-license">
-              Icons made by
-              <Link to="http://www.flaticon.com/authors/flat-icons"
-                title="Flat Icons"
+          </div>
+        </div>
+        <div className="row">
+          <div className="credits">
+            <div className="col-sm-12">
+              <div className="our-vendor">
+                &copy; All rights reserved&nbsp;|&nbsp;Photos by &copy;&nbsp;
+                <strong>
+                  <Link
+                    to="http://www.alicerowsome.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Alice Rowsome
+                  </Link>
+                </strong>
+                &nbsp;|&nbsp;Supported with{" "}
+                <img src="/img/ico/hearts.svg" alt="LOVE" />by&nbsp;
+                <strong>
+                  <Link
+                    to="http://www.hackyourfuture.net"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                  HackYourFuture
+                  </Link>&nbsp;|&nbsp;
+                </strong>
+              Registered charity number&nbsp;
+              <Link
+                to="http://apps.charitycommission.gov.uk/Showcharity/RegisterOfCharities/CharityFramework.aspx?RegisteredCharityNumber=1174929&SubsidiaryNumber=0"
                 target="_blank"
-                rel="noopener noreferrer">
-                Flat Icons</Link> from
-                <Link to="http://www.flaticon.com"
-                  title="Flaticon"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  www.flaticon.com
-                </Link> is licensed by
-                <Link to="http://creativecommons.org/licenses/by/3.0/"
-                  title="Creative Commons BY 3.0"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  CC 3.0 BY
-                </Link>
+                rel="noopener noreferrer"
+              >
+                1174929
+              </Link>.
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
-</div>
-)
+    </footer>
+  </div>
+);
