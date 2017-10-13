8a1ab9ec7071eed1080d0395142b95be92f3b858
MisaOgura <misa.ogura01@gmail.com>
2017-10-13 18:22:46 +0100

    Start extracting logo cards

--- src/components/PartnerLogos.js
+++ src/components/PartnerLogos.js
@@ -6,6 +6,14 @@ const Feature = styled.div`
   padding-bottom: 50px;
 `;
 
+const LogoCard = ({ href, src, alt }) => (
+  <div className="col-sm-4 col-md-4 col-sm-offset-2 col-md-offset-2">
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <img src={src} alt={alt} />
+    </a>
+  </div>
+)
+
 export const PartnerLogos = () => (
   <section className="section-container section-container-gray">
     <div className="container">
@@ -18,15 +26,23 @@ export const PartnerLogos = () => (
       </Feature>
 
       <div className="row">
-        <div className="col-sm-4 col-md-4 col-sm-offset-2 col-md-offset-2">
-          <a href={partners.cititec.url} target="_blank" rel="noopener noreferrer">
-            <img src={partners.cititec.logo} alt={partners.cititec.name} />
-          </a>
-        </div>
+        <LogoCard href={partners.cititec.url} src={partners.cititec.logo} alt={partners.cititec.name}/>
 
         <div className="col-sm-4 col-md-4">
           <a href={partners.pipedrive.url} target="_blank" rel="noopener noreferrer">
             <img width="240px" height="52.6px" src={partners.pipedrive.logo} alt={partners.pipedrive.name} />
+          </a>
+        </div>
+
+        <div className="col-sm-4 col-md-4 col-sm-offset-2 col-md-offset-2">
+          <a href={partners.github.url} target="_blank" rel="noopener noreferrer">
+            <img width="200" height="auto" src={partners.github.logo} alt={partners.github.name} />
+          </a>
+        </div>
+
+        <div className="col-sm-4 col-md-4">
+          <a href={partners.geckoboard.url} target="_blank" rel="noopener noreferrer">
+            <img src={partners.geckoboard.logo} alt={partners.geckoboard.name} />
           </a>
         </div>
       </div>
