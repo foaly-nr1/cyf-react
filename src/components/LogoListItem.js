fb45db09106eeefbadbab333fa2fe3faab2b0c6a
MisaOgura <misa.ogura01@gmail.com>
2017-10-14 17:43:32 +0100

    Make LogoListItem ready to be used

--- src/components/LogoListItem.js
+++ src/components/LogoListItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const LogoListItem = () => (
-  <div className="logo-list-item">
-    <a>
-      <img />
+const LogoListItem = ({ href, src, alt }) => (
+  <div className="logo-list-item col-sm-4 col-md-4">
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <img src={src} alt={alt} />
     </a>
   </div>
 )
--- src/components/LogoListItem.test.js
+++ src/components/LogoListItem.test.js
@@ -2,22 +2,41 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import LogoListItem from './LogoListItem'
 
+// configure enzyme
+// TODO: move to a separate file
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
 configure({ adapter: new Adapter() })
 
 describe('LogoListItem', () => {
+  const props = {
+    href: 'a-company-url',
+    src: 'logoImageSource.png',
+    alt: 'alternateName'
+  }
+
   it('renders a div with a className logo-list-item', () => {
     const logoListItem = shallow(<LogoListItem />)
-
     expect(logoListItem.hasClass('logo-list-item')).toEqual(true)
   })
 
   it('renders an a tag with an image', () => {
     const logoListItem = shallow(<LogoListItem />)
-    const aTag = logoListItem.find('a')
+    expect(logoListItem.find('a').find('img')).toHaveLength(1)
+  })
 
-    expect(aTag).toHaveLength(1)
-    expect(aTag.find('img')).toHaveLength(1)
+  it('renders an a tag with a link to a company url', () => {
+    const logoListItem = shallow(<LogoListItem {...props} />)
+    expect(logoListItem.find('a').prop('href')).toEqual(props.href)
+  })
+
+  it('renders an image from a specified source', () => {
+    const logoListItem = shallow(<LogoListItem {...props} />)
+    expect(logoListItem.find('a').find('img').prop('src')).toEqual(props.src)
+  })
+
+  it('renders an image with an alternate name', () => {
+    const logoListItem = shallow(<LogoListItem {...props} />)
+    expect(logoListItem.find('a').find('img').prop('alt')).toEqual(props.alt)
   })
 })
