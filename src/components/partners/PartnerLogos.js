3a91c42281d09f0cb93e76406cbaa978f8246ef6
MisaOgura <misa.ogura01@gmail.com>
2017-10-16 12:59:04 +0100

    PartnerLogos render a LogoList for each section of partners

--- src/components/partners/PartnerLogos.js
+++ src/components/partners/PartnerLogos.js
@@ -2,32 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import LogoList from './LogoList'
 
-const Feature = styled.div`
-  padding-bottom: 50px;
-`;
+// const Feature = styled.div`
+//   padding-bottom: 50px;
+// `;
 
-const renderFeaturedLogoList = (partners) => {
-  const featuredLogoList = partners
-    .filter(partner => partner.featured)
-    .map(partner => (
-      <Feature key={partner.id} className="row">
-        <div className="col-sm-10 col-sm-offset-1 ticketmaster-wrap">
-          <a href={partner.url} target="_blank" rel="noopener noreferrer">
-            <img className="img-fluid" src={partner.logo} alt={partner.name}/>
-          </a>
-        </div>
-      </Feature>
-    ));
+// const renderFeaturedLogoList = (partners) => {
+//   const featuredLogoList = partners
+//     .filter(partner => partner.featured)
+//     .map(partner => (
+//       <Feature key={partner.id} className="row">
+//         <div className="col-sm-10 col-sm-offset-1 ticketmaster-wrap">
+//           <a href={partner.url} target="_blank" rel="noopener noreferrer">
+//             <img className="img-fluid" src={partner.logo} alt={partner.name}/>
+//           </a>
+//         </div>
+//       </Feature>
+//     ));
+//
+//   return featuredLogoList;
+// };
 
-  return featuredLogoList;
-};
+const renderLogoListSections = (partners) => {
+  return Object.keys(partners).map(section => {
+    const className = `container partners__${section}`
 
+    return (
+      <div key={section} className={className}>
+        <LogoList partners={partners[section]}/>
+      </div>
+    )
+  })
+}
 
 export const PartnerLogos = ({ partners }) => (
   <section className="section-container section-container-gray">
-    <div className="container">
-      {renderFeaturedLogoList(partners)}
-      <LogoList partners={partners}/>
+    <div className="container partners__general">
+      {/*{renderFeaturedLogoList(partners)}*/}
+      {renderLogoListSections(partners)}
     </div>
   </section>
 );
--- src/components/partners/PartnerLogos.test.js
+++ src/components/partners/PartnerLogos.test.js
@@ -4,7 +4,7 @@ import { PartnerLogos } from './PartnerLogos';
 import LogoList from './LogoList';
 
 describe('PartnerLogos', () => {
-  const props = {
+  const propsa = {
     partners: [
       {
         id: 'featuredCompany',
@@ -30,17 +30,53 @@ describe('PartnerLogos', () => {
     ]
   };
 
+  const props = {
+    partners: {
+      sectionOne: [
+        {
+          id: 'featuredCompany',
+          name: 'Featured Company',
+          url: 'url-to-featured-company',
+          logo: 'logoForFeaturedCompany.png',
+          featured: true
+        },
+      ],
+      sectionTwo: [
+        {
+          id: 'nonFeaturedCompany1',
+          name: 'Non-featured Company 1',
+          url: 'url-to-non-featured-company-1',
+          logo: 'logoForNonFeaturedCompany1.png',
+          featured: false
+        },
+      ],
+      sectionThree: [
+        {
+          id: 'nonFeaturedCompany1',
+          name: 'Non-featured Company 1',
+          url: 'url-to-non-featured-company-1',
+          logo: 'logoForNonFeaturedCompany1.png',
+          featured: false
+        },
+      ],
+    },
+  };
+
   let partnerLogos;
+  const sections = Object.keys(props.partners)
 
   beforeEach(() => {
     partnerLogos = shallow(<PartnerLogos {...props} />)
   });
 
-  it('renders a LogoList', () => {
-    expect(partnerLogos.find(LogoList)).toHaveLength(1)
+  it('renders a LogoList for each section of partners', () => {
+    expect(partnerLogos.find(LogoList)).toHaveLength(sections.length)
   });
 
-  it('passes the partners as a props to LogoList', () => {
-    expect(partnerLogos.find(LogoList).prop('partners')).toEqual(props.partners)
-  });
+  sections.forEach(section => {
+    it(`passes the right props to ${section}'s LogoList`, () => {
+      const sectionDiv = partnerLogos.find(`.partners__${section}`)
+      expect(sectionDiv.find(LogoList).prop('partners')).toEqual(props.partners[section])
+    })
+  })
 });
