bdf1e93731f56e634299c4bd255996b989ac8dd3
Matt Paul <mattpaul001@icloud.com>
2017-11-03 07:19:51 +0000

    Serialize snapshots correctly

--- src/components/as-seen/index.js
+++ src/components/as-seen/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const mediaOutlets = [
+  {
+    url: 'http://www.wired.co.uk/article/codeyourfuture-refugee-coding-school',
+    src: 'img/media-logos/wired.svg',
+    alt: 'Wired'
+  },
+  {
+    url:
+      'http://www.unhcr.org/news/stories/2017/1/586e420c7/volunteers-train-refugees-to-crack-into-london-tech-industry.html',
+    src: 'img/media-logos/unhcr.png',
+    alt: 'UNHCR'
+  },
+  {
+    url:
+      'https://www.newsdeeply.com/refugees/articles/2016/10/19/welcome-to-londons-refugee-coding-school',
+    src: 'img/media-logos/refugees-deeply.jpeg',
+    alt: 'Refugees Deeply'
+  },
+  {
+    url: 'http://www.bbc.co.uk/programmes/p04yzrrg',
+    src: 'img/media-logos/BBC.svg',
+    alt: 'BBC'
+  }
+];
+
+const Outlet = ({ url, src, alt }) => (
+  <div className="col-sm-3">
+    <Link to={url} target="_blank" rel="noopener noreferrer">
+      <div className="icon--large">
+        <img src={src} alt={alt} />
+      </div>
+    </Link>
+  </div>
+);
+
 const AsSeen = () => (
   <div className="container">
     <div className="row">
@@ -13,53 +48,10 @@ const AsSeen = () => (
       </div>
     </div>
     <div className="row">
-      <div className="col-sm-3">
-        <Link
-          to="http://www.wired.co.uk/article/codeyourfuture-refugee-coding-school"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="icon--large">
-            <img src="img/media-logos/wired.svg" alt="Wired" />
-          </div>
-        </Link>
-      </div>
-      <div className="col-sm-3">
-        <Link
-          to="http://www.unhcr.org/news/stories/2017/1/586e420c7/volunteers-train-refugees-to-crack-into-london-tech-industry.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="icon--large">
-            <img src="img/media-logos/unhcr.png" alt="UNHCR" />
-          </div>
-        </Link>
-      </div>
-      <div className="col-sm-3">
-        <Link
-          to="https://www.newsdeeply.com/refugees/articles/2016/10/19/welcome-to-londons-refugee-coding-school"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="icon--large">
-            <img
-              src="img/media-logos/refugees-deeply.jpeg"
-              alt="Refugees Deeply"
-            />
-          </div>
-        </Link>
-      </div>
-      <div className="col-sm-3">
-        <Link
-          to="http://www.bbc.co.uk/programmes/p04yzrrg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="icon--large">
-            <img src="img/media-logos/BBC.svg" alt="BBC" />
-          </div>
-        </Link>
-      </div>
+      {mediaOutlets &&
+        mediaOutlets.map(x => (
+          <Outlet key={x.src} src={x.src} url={x.url} alt={x.alt} />
+        ))}
     </div>
   </div>
 );
