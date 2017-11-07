9f431a4e0c2c1df3862940326654f198dcd861d9
Felix Meziere <felix.meziere@gmail.com>
2017-11-07 09:07:30 +0000

    Remove Stripe Credentials from Circle-Ci

--- ci/deploy_lambda.sh
+++ ci/deploy_lambda.sh
@@ -5,10 +5,10 @@ BRANCH="$1"
 # Changes variables in code depending on environment.
 if [ $BRANCH == "production" ]; then
     sed -i -e "s#__REACT_APP_STRIPE_PUBLIC_KEY__#pk_live_wvm6YFWTG1FrYAGVuzCLIPx8#g" .env
-	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://b7g163fmob.execute-api.eu-west-1.amazonaws.com/production/donate#g" .env
+	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://ivc9bvmb2d.execute-api.eu-west-1.amazonaws.com/production/donate#g" .env
 	cd lambda && yarn && yarn deploy-production
 else
 	sed -i -e "s#__REACT_APP_STRIPE_PUBLIC_KEY__#pk_test_noH4KPBQ7w5U3wZaRVo6NbmS#g" .env
-	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://kdgpfxm32b.execute-api.eu-west-1.amazonaws.com/staging/donate#g" .env
+	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://fsdynf2mse.execute-api.eu-west-1.amazonaws.com/staging/donate#g" .env
 	cd lambda && yarn && yarn deploy-staging
 fi
