0836e67250061aee2e4ceb9143465047f72d128e
Felix Meziere <felix.meziere@gmail.com>
2017-11-07 09:52:51 +0000

    Stripe secrets in parameter store

--- lambda/functions/stripe.js
+++ lambda/functions/stripe.js
@@ -1,23 +1,28 @@
-// config
-const STRIPE_PRIVATE = process.env.STRIPE_PRIVATE;
+const ssm = require('../tools/ssm');
 
-// deps
-const stripe = require('stripe')(STRIPE_PRIVATE);
+// config
 
 module.exports = (event, context, callback)  => {
   if (event.method === 'POST') {  
     const { token, amount } = event.body;
-    stripe.charges
-    .create({
-      amount: amount * 100,
-      currency: 'gbp',
-      description: 'CYF Donation',
-      source: token
-    })
-    .then(() => callback(null, '200'))
-    .catch(err => {
-      console.log(err);
-      callback(500, err)
-    })
+    ssm.getParameter(process.env.STRIPE_PRIVATE_KEY_ALIAS)
+       .then(stripe_private_key => {
+          return require('stripe')(stripe_private_key);
+        }
+      ).then(stripe => {
+        return stripe.charges
+        .create({
+          amount: amount * 100,
+          currency: 'gbp',
+          description: 'CYF Donation',
+          source: token
+        })
+        .then(() => callback(null, '200'))
+        .catch(err => {
+          console.log(err);
+          callback(500, err)
+        })
+      }
+    )
   }
 };
