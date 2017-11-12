a5c76cc21ed902bb0a918dfd204edd54300c1292
Matt Paul <mattpaul001@icloud.com>
2017-11-12 12:25:05 +0000

    Save CYF event to dynamoDb

---
+++ lambda/events/create.js
@@ -0,0 +1,25 @@
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+/* eslint-disable import/prefer-default-export */
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'events',
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      eventId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: new Date().getTime(),
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call('put', params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
