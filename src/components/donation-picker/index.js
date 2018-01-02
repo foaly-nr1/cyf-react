ed5dbd42732c8c4da2bf4ae72eb9c903966d4cfe
Felix Meziere <felix.meziere@gmail.com>
2018-01-02 14:42:00 +0000

    New donation page

--- src/components/donation-picker/index.js
+++ src/components/donation-picker/index.js
@@ -1,36 +1,101 @@
 import React from 'react';
-import {
-  FormControl,
-  InputGroup,
-  FormGroup,
-  ControlLabel,
-  Button,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { FormControl, InputGroup, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+
+const Container = styled('div')`
+  max-width: 420px;
+`;
+
+const PeriodButtonsContainer = styled('div')`
+  display: flex;
+`;
+
+const PeriodButton = styled('label')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 34px;
+  width: 50%;
+  font-weight: 900;
+  background-color: ${props => (props.active ? 'rgb(43, 165, 96)' : null)};
+  color: ${props => (props.active ? 'white' : null)};
+  border-radius: ${props => (props.isLeft ? '6px 0 0 6px' : '0 6px 6px 0')};
+  border: solid;
+  border-width: ${props =>
+    props.isLeft ? '1px 0px 1px 1px' : '1px 1px 1px 0px'};
+  border-color: rgb(204, 204, 204);
+`;
+
+const PickerContainer = styled('div')`
+  margin-top: 20px;
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DonationLabel = styled('div')`
+  background-color: rgb(244, 245, 240);
+  border-style: solid;
+  border-width: 0.5px 0.5px 0px 0.5px;
+  border-color: rgb(204, 204, 204);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  font-weight: 1000;
+  font-size: 1.6rem;
+  padding: 16px;
+`;
+
+const DonationBox = styled('div')`
+  border-style: solid;
+  border-width: 0.5px 0.5px 0.5px 0.5px;
+  border-color: rgb(204, 204, 204);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  flex-direction: column;
+`;
+
+const InputContainer = styled('div')`
+  width: 90%;
+  margin-bottom: 24px;
+`;
+
+const DonateButton = styled(Button)`
+  width: 90%;
+  font-size: 2.2rem;
+  font-weight: 900;
+  height: 200px;
+`;
+
+const MonthlyMessageContainer = styled('div')`
+  width: 80%;
+`;
 
 class DonationPicker extends React.Component {
-  propTypes = {
-    onTokenCapture: PropTypes.func,
-  };
-
   state = {
     amount: 20,
     // eslint-disable-next-line react/no-unused-state
     token: null,
+    isOnceButtonSelected: true,
   };
 
   componentDidMount() {
     setTimeout(() => {
-      this.stripeHandler = window.StripeCheckout.configure({
-        key: process.env.REACT_APP_STRIPE_PUBLIC_KEY,
-        image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-        locale: 'auto',
-        token: this.onTokenSuccess,
-        currency: 'GBP',
-        bitcoin: true,
-      });
+      if (window.StripeCheckout) {
+        this.stripeHandler = window.StripeCheckout.configure({
+          key: process.env.REACT_APP_STRIPE_PUBLIC_KEY,
+          image:
+            'https://stripe.com/img/documentation/checkout/marketplace.png',
+          locale: 'auto',
+          token: this.onTokenSuccess,
+          currency: 'GBP',
+          bitcoin: true,
+        });
+      }
     }, 1000);
   }
 
@@ -62,34 +127,82 @@ class DonationPicker extends React.Component {
 
   render() {
     const { amount } = this.state;
+    const labelText = this.state.isOnceButtonSelected
+      ? 'How much would you like to give?'
+      : 'How much would you like to give every month?';
+
     return (
-      <div>
-        <Row>
-          <Col xs={12} md={6}>
-            <FormGroup controlId="donationForm">
-              <ControlLabel>How much would you like to give?</ControlLabel>
-              <InputGroup>
-                <InputGroup.Addon>&pound;</InputGroup.Addon>
-                <FormControl
-                  type="number"
-                  value={amount}
-                  onChange={this.updateAmount}
-                />
-              </InputGroup>
-              <FormControl.Feedback />
-            </FormGroup>
-          </Col>
-          <Col xs={12} md={6}>
-            <div>
-              <Button size="small" onClick={this.checkout}>
-                Donate
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </div>
+      <Container>
+        <PeriodButtonsContainer>
+          <PeriodButton
+            active={this.state.isOnceButtonSelected}
+            onClick={() => this.setState({ isOnceButtonSelected: true })}
+            isLeft
+          >
+            GIVE ONCE
+          </PeriodButton>
+          <PeriodButton
+            active={!this.state.isOnceButtonSelected}
+            onClick={() => this.setState({ isOnceButtonSelected: false })}
+          >
+            MONTHLY
+          </PeriodButton>
+        </PeriodButtonsContainer>
+        <PickerContainer>
+          <DonationLabel>{labelText}</DonationLabel>
+          {this.state.isOnceButtonSelected && (
+            <DonationBox>
+              <InputContainer>
+                <InputGroup
+                  style={{
+                    'font-size': '2.2rem',
+                  }}
+                >
+                  <InputGroup.Addon
+                    style={{
+                      'font-size': '2.2rem',
+                    }}
+                  >
+                    &pound;
+                  </InputGroup.Addon>
+                  <FormControl
+                    type="number"
+                    value={amount}
+                    onChange={this.updateAmount}
+                    style={{
+                      height: '60px',
+                      'font-size': '2.2rem',
+                    }}
+                  />
+                </InputGroup>
+              </InputContainer>
+              <DonateButton
+                onClick={this.checkout}
+                bsStyle={{
+                  height: '200px',
+                  'font-weight': '900',
+                }}
+              >
+                DONATE
+              </DonateButton>
+            </DonationBox>
+          )}
+          {!this.state.isOnceButtonSelected && (
+            <DonationBox>
+              <MonthlyMessageContainer>
+                Monthly donations will be available soon! <br />
+                <br /> You can do a one-off donation in the meantime by clicking
+                GIVE ONCE.
+              </MonthlyMessageContainer>
+            </DonationBox>
+          )}
+        </PickerContainer>
+      </Container>
     );
   }
 }
 
+DonationPicker.propTypes = {
+  onTokenCapture: PropTypes.func.isRequired,
+};
 export default DonationPicker;
