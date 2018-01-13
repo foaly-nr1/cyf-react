40f801af311077b1401c998f84a3759d492f3d2d
Matt Paul <mattpaul001@icloud.com>
2018-01-13 16:31:31 +0000

    Fix up

--- lambda/events/functions/create.js
+++ lambda/events/functions/create.js
@@ -8,18 +8,24 @@ export async function main(event, context, callback) {
   const params = {
     TableName: 'events',
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: event.requestContext.identity.cognitoIdentityId || 'CYFMentor',
       eventId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
+      title: data.body.title,
+      description: data.body.description,
+      startDate: data.body.startDate,
+      endDate: data.body.endDate,
+      city: data.body.city,
+      location: data.body.location,
       createdAt: new Date().getTime(),
     },
   };
 
+  console.log(params);
   try {
     const result = await dynamoDbLib.call('put', params);
     callback(null, success(params.Item));
   } catch (e) {
+    console.log(e);
     callback(null, failure({ status: false }));
   }
 }
