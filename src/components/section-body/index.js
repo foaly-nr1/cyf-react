d66faa5f1c851103234b010cdab25da302afac89
Matt Paul <mattpaul001@icloud.com>
2018-01-20 08:24:02 +0000

    Add styling

--- src/components/section-body/index.js
+++ src/components/section-body/index.js
@@ -1,11 +1,18 @@
 // @flow
 import React from 'react';
+import styled from 'react-emotion';
 import type { ChildrenArray, Element } from 'react';
 
 type Props = {
   children: ChildrenArray<Element<'p'>>,
 };
 
-const SectionBody = ({ children }: Props) => <div>{children}</div>;
+const Container = styled('div')`
+  & p {
+    font-size: 18px;
+    text-align: left;
+  }
+`;
+const SectionBody = ({ children }: Props) => <Container>{children}</Container>;
 
 export default SectionBody;
