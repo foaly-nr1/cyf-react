32a6f2f3d8c6bbbe4e622dbed50ef42c87649284
Matt Paul <mattpaul001@icloud.com>
2018-01-20 14:27:03 +0000

    Update section heading component

--- src/components/section-heading/index.js
+++ src/components/section-heading/index.js
@@ -1,11 +1,19 @@
 // @flow
 import React from 'react';
+import styled from 'react-emotion';
 import type { ChildrenArray, Element } from 'react';
 
 type Props = {
   children: ChildrenArray<Element<'h1' | 'h2' | 'h3'>>,
 };
 
-const SectionHeading = ({ children }: Props) => <div>{children}</div>;
+const StyledHeading = styled('div')`
+  & h3 {
+    text-align: left;
+  }
+`;
+const SectionHeading = ({ children }: Props) => (
+  <StyledHeading>{children}</StyledHeading>
+);
 
 export default SectionHeading;
