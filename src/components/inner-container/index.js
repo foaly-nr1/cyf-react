1143fd6dbfc9dede6066c9a8bf9f8d288dc948c7
Matt Paul <mattpaul001@icloud.com>
2018-01-20 14:27:45 +0000

    Update inner container component

--- src/components/inner-container/index.js
+++ src/components/inner-container/index.js
@@ -3,8 +3,12 @@ import styled from 'react-emotion';
 
 const InnerContainer = styled('div')`
   max-width: calc(900px + 64px);
+  outline: 1px solid red;
   margin: 0 auto;
-  padding: 32px;
+  padding: 8px;
+  // @media (--large) {
+  //   padding: 32px;
+  // }
 `;
 
 export default InnerContainer;
