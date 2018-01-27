6827b2255c293cf252e51b4dfeaaa04761242bf8
Matt Paul <mattpaul001@icloud.com>
2018-01-27 14:27:03 +0000

    Event rendering to page

--- src/pages/event/index.test.js
+++ src/pages/event/index.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Event from './';
+import { Event } from './';
+
+const mockEvent = {
+  city: 'London',
+  description: 'First bit of javascript',
+  endDate: '1515851599504',
+  intake: 'London Class 3',
+  links: [
+    {
+      'course materials':
+        'https://codeyourfuture.github.io/syllabus-master/js-core/week-04/lesson.html',
+    },
+  ],
+  location: 'Ticketmaster',
+  mentors: [
+    { name: 'Matt', avatar: 'www.something.com' },
+    { name: 'Jun', avatar: 'www.somthing.com' },
+    { name: 'Felix', avatar: 'www.somthing.com' },
+  ],
+  moduleLeaders: [{ name: 'Kash', avatar: 'something' }],
+  startDate: '1515851599504',
+  topic: 'Week 4 - Hello Javascript',
+};
 
 describe('Event Component', () => {
   it('renders', () => {
-    expect(shallow(<Event />)).toMatchSnapshot();
+    expect(shallow(<Event {...mockEvent} />)).toMatchSnapshot();
   });
 });
