a96c4ed0d8c10774eb13989f003b19aefd8ef055
Dominik Piatek <do.piatek@gmail.com>
2018-01-31 14:31:20 +0000

    Install with freeze and use newer npm version
    This provides a package-lock.json which necessary because of
    buggy serverless-webpack behaviour. The plugin runs `npm install`
    during serverless deploy for bundling, ignorning yarn.lock.
    
    This is only a temp solution - we should not depend on package
    that can't resolve our dependencies correctly.

--- ci/deploy_lambda.sh
+++ ci/deploy_lambda.sh
@@ -8,11 +8,11 @@ if [ $BRANCH == "production" ]; then
 	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://ivc9bvmb2d.execute-api.eu-west-1.amazonaws.com/production/donate#g" .env
 	sed -i -e "s#__REACT_APP_PIPEDRIVE_ENDPOINT__#https://ivc9bvmb2d.execute-api.eu-west-1.amazonaws.com/production/database#g" .env
 	export ENVIRONMENT=production
-	cd lambda && yarn && yarn deploy-production && cd events && yarn && yarn deploy
+	cd lambda && yarn --frozen-lockfile && yarn deploy-production && cd events && yarn --frozen-lockfile && yarn deploy
 else
 	sed -i -e "s#__REACT_APP_STRIPE_PUBLIC_KEY__#pk_test_noH4KPBQ7w5U3wZaRVo6NbmS#g" .env
 	sed -i -e "s#__REACT_APP_DONATION_ENDPOINT__#https://fsdynf2mse.execute-api.eu-west-1.amazonaws.com/staging/donate#g" .env
 	sed -i -e "s#__REACT_APP_PIPEDRIVE_ENDPOINT__#https://fsdynf2mse.execute-api.eu-west-1.amazonaws.com/staging/database#g" .env
 	export ENVIRONMENT=staging
-	cd lambda && yarn && yarn deploy-staging && cd events && yarn && yarn deploy
+	cd lambda && yarn --frozen-lockfile && yarn deploy-staging && cd events && yarn --frozen-lockfile && yarn deploy
 fi
