b9b30dcc48acc382f5b4bb072d8ca83a550b67fd
Matt Paul <mattpaul001@icloud.com>
2018-02-01 18:40:58 +0000

    Mobile styling for event list item

--- src/components/event-card/index.js
+++ src/components/event-card/index.js
@@ -3,24 +3,46 @@ import React from 'react';
 import styled from 'react-emotion';
 import type { CYFEvent } from '../../types';
 
-const Container = styled('div')`
-  background-color: white;
-  text-align: left;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
-  padding: 8px;
-`;
+const Container = styled('div')({
+  backgroundColor: 'white',
+  textAlign: 'left',
+  boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.5)',
+  padding: '12px',
+  '*': {
+    margin: '0' /* Overide all global css horribleness from legacy.css */,
+  },
+  h4: {
+    color: '#333333',
+    fontSize: '18px',
+    paddingBottom: '12px',
+  },
+  'p,span': {
+    color: '#666666',
+    lineHeight: '20px',
+  },
+});
 
-const Topic = styled('p')`
-  font-weight: 700;
-`;
+const Location = styled('div')({
+  paddingBottom: '12px',
+  span: {
+    fontSize: '14px',
+  },
+});
 
-const Attendance = styled('span')`
-  float: right;
-`;
+const Topic = styled('p')({
+  fontWeight: '700',
+  fontSize: '16px',
+});
 
-const EventLink = styled('a')`
-  text-decoration: underline;
-`;
+const Attendance = styled('span')({
+  float: 'right',
+  fontSize: '16px',
+});
+
+const EventLink = styled('a')({
+  textDecoration: 'underline',
+  fontSize: '16px',
+});
 
 const EventCard = ({
   city,
@@ -36,16 +58,18 @@ const EventCard = ({
   <Container>
     <h4>{intake}</h4>
     <Topic>{topic}</Topic>
-    <p>
-      <span>{city}</span>
-      <span>, </span>
-      <span>{location}</span>
-    </p>
-    <p>
-      <span>{startTime}</span>
-      <span> -</span>
-      <span>{endTime}</span>
-    </p>
+    <Location>
+      <p>
+        <span>{city}</span>
+        <span>, </span>
+        <span>{location}</span>
+      </p>
+      <p>
+        <span>{startTime}</span>
+        <span> -</span>
+        <span>{endTime}</span>
+      </p>
+    </Location>
     <EventLink href={`/events/${eventId}`}>View event</EventLink>
     <Attendance>
       <span>{mentors.length + moduleLeaders.length}</span>
