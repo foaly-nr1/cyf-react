0748cbed0cbea7ec040e44c3cda59f5f349192f1
Amit Shah <amit@amwam.me>
2018-03-25 21:27:23 +0100

    udpated to the new production events api

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -17,7 +17,7 @@ if [ $BRANCH == "staging" ]; then
     REPO_NAME="web-static-staging"
 elif [ $BRANCH == "production" ]; then
     export REACT_APP_HOST=$HOST_PRODUCTION
-    export REACT_APP_EVENTS_URL="https://malsy22v0j.execute-api.eu-west-1.amazonaws.com/production/events"
+    export REACT_APP_EVENTS_URL="https://mipzdw4o0c.execute-api.eu-west-1.amazonaws.com/production/events"
     export REACT_APP_STRIPE_PUBLIC_KEY=pk_live_wvm6YFWTG1FrYAGVuzCLIPx8
     export REACT_APP_DONATION_ENDPOINT=https://fsdynf2mse.execute-api.eu-west-1.amazonaws.com/staging/donate
     export REACT_APP_PIPEDRIVE_ENDPOINT=https://fsdynf2mse.execute-api.eu-west-1.amazonaws.com/staging/database
