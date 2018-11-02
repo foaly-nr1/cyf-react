c23bfc72ce28396a908a7484cbd3028871f77d18
Kash <kash.karimi@gmail.com>
2018-11-02 15:23:37 +0000

    update snapshots

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -59,9 +59,6 @@ const Navigation = ({ auth }: Props) => (
         <LinkContainer to="/meet-the-team">
           <NavItem eventKey={1}>Meet the team</NavItem>
         </LinkContainer>
-        <LinkContainer to="/conference">
-          <NavItem eventKey={1}>Conference</NavItem>
-        </LinkContainer>
         <NavItem className="nav-icon" eventKey="blog">
           Blog
         </NavItem>
