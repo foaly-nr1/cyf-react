3f56bca03379c3ce9a2ab82173f269343d5c633b
Bram Borggreve <borggreve@gmail.com>
2018-11-16 19:54:29 -0500

    add flyer and links for CYFCO campaign

--- src/pages/colombia/index.js
+++ src/pages/colombia/index.js
@@ -1,35 +1,41 @@
 // @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'react-emotion';
 
 import TopSection from '../../components/top-section';
-import colombialogo from '../../assets/images/cyf-colombia-logo.png';
+import colombiaFlyer from '../../assets/images/cyf-colombia-flyer.jpg';
+import colombiaLogo from '../../assets/images/cyf-colombia-logo.png';
+
+const studentLink = 'https://airtable.com/shr8vflbZQkh5hAe4';
+const volunteerLink = 'https://airtable.com/shroNkIefDJfFksXa';
+
+const ActionLink = styled('h2')`
+  color: #6067af;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 const ColombiaText = (
   <div>
-    <p>
-      Code Your Future is a non-profit organisation supporting refugees with the
-      dream of becoming developers. In their journey of interrupted lives,
-      unfinished studies and integration challenges, many asylum seekers and
-      refugees yearn to update their tech skills, but lack learning
-      opportunities. We want to change this by offering a program in which they
-      are mentored by professional developers in order to prepare them to be
-      able to work as software developers.
-    </p>
-    <p>
-      Launched in the UK in 2017, Code Your Future has now expanded to South
-      Africa, under the leadership of co-founders Absolom Mugwagwa and Deon
-      Petersen.
-    </p>
-    <p>
-      This is just the beginning and with your help we will be expanding to
-      other regions and cities.
-    </p>
-    <p>
-      If you are interested in participating as a{' '}
-      <Link to="students">student</Link>,{' '}
-      <Link to="volunteers">coach or volunteer</Link>, sign up!
-    </p>
+    <div>
+      <img
+        src={colombiaFlyer}
+        alt="Code Your Future Colombia"
+        style={{ margin: '75px 0' }}
+      />
+    </div>
+
+    <ActionLink>
+      <a href={studentLink}>Quiero ser estudiante</a>
+    </ActionLink>
+
+    <ActionLink>
+      <a href={volunteerLink}>Quiero ser voluntario</a>
+    </ActionLink>
+
     <p>
       For all other inquiries please contact us at{' '}
       <Link to="mailto:colombia@codeyourfuture.io" title="Contact us">
@@ -43,10 +49,12 @@ const ColombiaText = (
 
 const Colombia = () => (
   <main>
-    <div>
-      <img src={colombialogo} alt="Code Your Future Colombia" />
-    </div>
     <TopSection title="Code Your Future Colombia" content={ColombiaText} />
+    <img
+      src={colombiaLogo}
+      alt="Code Your Future Colombia"
+      style={{ width: '200px' }}
+    />
   </main>
 );
 
